8eaa1183a787f22d301ab4f5db8cb5eb8f81ed88
Naufal-DevVoder <75933350+Naufal-DevVoder@users.noreply.github.com>
2020-12-31 14:17:31 +0000

    Update config.js

--- config.js
+++ config.js
@@ -1,5 +1,5 @@
 exports.Token = ""; //Bot Token - Important
-exports.Owner = ""; //Bot Owner ID - Important
-exports.Default_Prefix = ""; //Bot Default Prefix (Examples: A!, !)- Important
-exports.Color = ""; //Bot All Embeds Color - Use CAPS For Name (Examples: BLUE, RANDOM) - Important
-exports.Support = ``; //Support Server Link - Never Gonna Give You Up (If No Link Provided)
+exports.Owner = "710458947529867345"; //Bot Owner ID - Important
+exports.Default_Prefix = "ww."; //Bot Default Prefix (Examples: A!, !)- Important
+exports.Color = "RANDOM"; //Bot All Embeds Color - Use CAPS For Name (Examples: BLUE, RANDOM) - Important
+exports.Support = `https://saweria.co/BlaZary`; //Support Server Link - Never Gonna Give You Up (If No Link Provided)
